4a29f8d05ba4d2dc9dfe53dbe532691c36dce991
SeiamAlMahmud <samahmud49@gmail.com>
2024-08-07 15:15:43 +0000

    login and log out

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -44,7 +44,7 @@ const signupUser = async (req, res) => {
 
             await newUser.save();
 
-            res.status(201).json({
+            res.status(200).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
                 username: newUser.username,
@@ -61,13 +61,43 @@ const signupUser = async (req, res) => {
     }
 }
 
-const loginUser = (req, res) => {
-    res.send("loginUser")
+const loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid username and password" })
+        }
+
+        generateTokenAndSetCookie(user._id, res)
+
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            email: user.email,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+        console.log("Error in Signin Controller", error)
+        res.status(500).json({ error: "Internal Server Error", message: error.message })
+    }
 }
 
 
-const logOutUser = (req, res) => {
-    res.send("logout User")
+const logOutUser =  (req, res) => {
+    try {
+
+        res.cookie("jwt","", {maxAge: 0});
+        res.status(200).json({message: "Log Out successfully"})
+    } catch (error) {
+        console.log("Error in Signin Controller", error)
+        res.status(500).json({ error: "Internal Server Error", message: error.message })
+    }
 }
 
 
