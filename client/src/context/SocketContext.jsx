4b45ce381754d08b5c1175822563782748d9c421
SeiamAlMahmud <samahmud49@gmail.com>
2024-08-16 11:56:19 +0000

    socket.io url updated

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:3000/", {
+            const socket = io("https://mern-chat-app-y0qi.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
