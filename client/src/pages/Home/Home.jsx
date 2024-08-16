3beacb33f60a1fec74f7a82d295a70157886a43e
SeiamAlMahmud <samahmud49@gmail.com>
2024-08-16 11:32:54 +0000

    title fixed

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
       navigate("/login") 
    }
   },[token])
+  document.title = "Mern Chat App"
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <Sidebar />
